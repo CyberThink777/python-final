9b98ab55a09b85526093f35cfafae74243adcd58
CyberThink777 <alexanderm61@gmail.com>
2020-10-28 19:39:19 +0700

    art.py: ascii art

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import art as art
+
 def main():
-    print("hello")
+    print("-----Welcome to Hangman-----")
+    print()
+    art.print_home()
+    print("----------------------------")
+
 if __name__ == "__main__":
     main()
