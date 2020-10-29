141435b2f457ace72b233a5ff3bbddf959fc989b
CyberThink777 <alexanderm61@gmail.com>
2020-10-29 08:21:35 +0700

    main: main looper

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ def main():
     art.print_home()
     print("----------------------------")
 
+    while True:
+        play_again = input("Do you want to play again?")
+        if play_again.lower() == "n":
+            break
+    else: 
+        print("Thanks for playing")
+
 if __name__ == "__main__":
     main()
