3df776ad0d60fae7ebe87e2c6d8c34a91ee586de
CyberThink777 <alexanderm61@gmail.com>
2020-10-30 23:36:38 +0700

    main.py: cleaning

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ def main():
     print()
     art.print_home()
     print("-------Topic: Fruits--------")
-    input("Press Enter to play")
+    input("Press Enter to play ")
 
     while True:
         game = Game()
         game.play()
         play_again = input("Do you want to play again?(y/n) ")
         if play_again.lower() == "n":
+            print("Thanks for playing")
             break
-    else:
-        print("Thanks for playing")
+        print()
 
 
 if __name__ == "__main__":
