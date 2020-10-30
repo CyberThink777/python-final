c8b2831295fa256e3e0552ccf32be4efc0615417
CyberThink777 <alexanderm61@gmail.com>
2020-10-30 20:15:48 +0700

    game.py: wip

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import art
 from word import get_word
 
 
@@ -6,7 +7,43 @@ class Game:
         super().__init__()
         self.word = get_word()
         self.state = 0
+        self.guessed_chars = set()
+        self.answer = ["_"] * len(self.word)
+
+    def print_answer(self):
+        for char in self.answer:
+            print("{} ".format(char), end="")
+        print()
+
+    def check_win(self):
+        return False
+        # if state < 10 and :
+
+    def check_lose(self):
+        return False
+        # if state < 10 and :
 
     def play(self):
-        print(self.word)
-        print(self.state)
+        while True:
+            # Win check
+            if self.check_win():
+                print("You win!")
+                break
+            if self.check_lose():
+                print("You lose!")
+                break
+
+            # Print hangman art
+            art.print_art(self.state)
+            self.print_answer()
+
+            guess = input("Write your guess: ")
+            # input validation
+            if not guess.isalpha() or len(guess) > 1:
+                print("Please write a single letter")
+                continue
+            if guess in self.guessed_chars:
+                print("You already chosen that letter")
+                continue
+            
+            self.guessed_chars.add(guess)
